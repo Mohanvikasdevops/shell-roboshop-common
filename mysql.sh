c3ae46b136baf28b8b584c652334ff824c2a2653
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-23 19:57:08 +0530

    actions

--- mysql.sh
+++ mysql.sh
@@ -4,13 +4,11 @@ source ./common.sh
 app_name=mysql
 
 check_root
-app_setup
-systemd_setup
-
-print_total_time
 
 dnf install mysql-server -y &>>$LOGS_FILE
 VALIDATE $? "Installed MySQL server"
+
+systemd_setup
 
 #get the password from user
 mysql_secure_installation --set-root-pass RoboShop@1
