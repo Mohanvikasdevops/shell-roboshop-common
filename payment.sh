15b61c4913f2e78af4dd617dddc4f8b876ac0775
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-23 22:14:18 +0530

    actions

--- payment.sh
+++ payment.sh
@@ -6,14 +6,6 @@ app_name=payment
 
 check_root
 app_setup
-
-dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
-VALIDATE $? "Installing Python"
-
-cd /app 
-pip3 install -r requirements.txt &>>$LOGS_FILE
-VALIDATE $? "Installing dependecies"
-
+python_setup
 systemd_setup
-
 print_total_time
