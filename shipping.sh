ceb875dbc49dbfb5af68e433f96e231d59cfcead
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-23 22:07:49 +0530

    actions

--- shipping.sh
+++ shipping.sh
@@ -4,8 +4,9 @@ source ./common.sh
 app_name=shipping
 
 check_root
-java_setup
 app_setup
+java_setup
+systemd_setup
 
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
@@ -21,6 +22,6 @@ else
     echo -e "data is already loaded ... $Y SKIPPING $N"
 fi
 
-systemd_setup
+app_restart
 
 print_total_time
