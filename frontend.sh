8916c67498d16bbca759525380ea5bb070ad6fdb
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-23 21:26:50 +0530

    actions

--- frontend.sh
+++ frontend.sh
@@ -7,11 +7,21 @@ version=1.24
 
 check_root
 app_code_setup
-systemd_setup
+
+systemctl enable nginx &>>$LOGS_FILE
+systemctl start nginx
+VALIDATE $? "Starting and enabling nginx"
 
 rm -rf /usr/share/nginx/html/*  &>>$LOGS_FILE
 VALIDATE $? "Removing existing code"
 
 app_setup
+
+rm -rf /etc/nginx/nginx.conf  &>>$LOGS_FILE
+VALIDATE $? "Removing existing code"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOGS_FILE
+VALIDATE $? "Copied our nginx conf file"
+
 app_restart
 print_total_time
