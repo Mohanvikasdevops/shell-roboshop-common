cc986f8990a5646c17c4cae22530be46aacb83d9
Mohanvikas <vikas.gonugunta@gmail.com>
2026-03-23 20:21:24 +0530

    actions

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -4,10 +4,13 @@ source ./common.sh
 app_name=rabbitmq
 
 check_root
-app_code_setup
+
 
 cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOGS_FILE
 VALIDATE $? "Added rabbitmq repo"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "Installing Rabbitmq server"
 
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
