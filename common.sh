389b54e75f8b7931ed49591bb1964ac799e799fe
Mohanvikas <vikas.gonugunta@gmail.com>
2026-04-16 12:26:03 +0530

    actions

--- common.sh
+++ common.sh
@@ -101,6 +101,15 @@ java_setup(){
     VALIDATE $? "Moving and Renaming $app_name" 
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Python"
+
+    cd /app 
+    pip3 install -r requirements.txt &>>$LOGS_FILE
+    VALIDATE $? "Installing dependecies"
+}
+
 sed_setup(){
     sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/$app_name/$app_name.conf &>>$LOGS_FILE
     VALIDATE $? "Allowing remote connections"
--- frontend.sh
+++ frontend.sh
@@ -5,6 +5,7 @@ app_name=frontend
 app_code=nginx
 version=1.24
 
+
 check_root
 app_code_setup
 
@@ -12,10 +13,17 @@ systemctl enable nginx &>>$LOGS_FILE
 systemctl start nginx
 VALIDATE $? "Starting and enabling nginx"
 
-rm -rf /usr/share/nginx/html/*  &>>$LOGS_FILE
+rm -rf $app_dir*  &>>$LOGS_FILE
 VALIDATE $? "Removing existing code"
 
-app_setup
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOGS_FILE
+VALIDATE $? "Downloading $app_name Code"
+
+cd $app_dir
+VALIDATE $? "Moving to app directory" 
+
+unzip /tmp/$app_name.zip &>>$LOGS_FILE
+VALIDATE $? "Unzip $app_name code"
 
 rm -rf /etc/nginx/nginx.conf  &>>$LOGS_FILE
 VALIDATE $? "Removing existing code"
